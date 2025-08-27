1109caa8af8867f0eb5f166ff8d56bf2307f2fbc
Alexey Gordeev <krevetkanos@gmail.com>
2025-08-27 15:18:24 +0000

    upd

---
+++ scripts.sh
@@ -0,0 +1,44 @@
+apk update
+apk upgrade
+apk add nano
+git --list
+echo pwd
+echo $PWD
+ls
+ls -l
+ls -la
+apk add git openssh
+apk update
+apk add openssh
+ssh keygen
+ssh-keygen
+cat ~/.ssh/id_rsa.pub > /dev/clipboard
+git clone git@github.com:pampaparam/Gordeev.git
+apk add git
+ls
+ls -la
+cat .ash_history
+echo $PWD
+ls -la
+git clone git@github.com:pampaparam/Gordeev.git
+cat ~/.ssh/id_rsa.pub > /dev/clipboard
+git clone git@github.com:pampaparam/Gordeev.git
+ls -la .ssh
+cat .ssh/id_rsa.pub
+cat ~/.ssh/id_rsa.pub > /dev/clipboard
+git clone git@github.com:pampaparam/Gordeev.git
+ls -la
+cd Gordeev
+ls -la
+apk add nano
+nano README.md
+cat README.md
+ ~/.ashrc
+echo 'alias c="git add . && git commit -m upd && git push origin main"' > ~/.ashrc
+source ~/.ashrc
+git status
+c
+git config --global user.name "Alexey Gordeev"
+git config --global user.email "krevetkanos@gmail.com"
+c
+cp ~/.ash_history scripts.sh
