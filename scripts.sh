3355c29a4597add7698ce7f35da636947423787f
alexeygordeev <krevetkanos@gmail.com>
2025-09-09 13:42:41 +0800

    upd

--- scripts.sh
+++ scripts.sh
@@ -1,44 +1,190 @@
-apk update
-apk upgrade
-apk add nano
-git --list
-echo pwd
-echo $PWD
+apt-get install git
+$PWD
+cd /
+$PWD
+apt-get install git
 ls
-ls -l
-ls -la
-apk add git openssh
-apk update
-apk add openssh
-ssh keygen
-ssh-keygen
-cat ~/.ssh/id_rsa.pub > /dev/clipboard
-git clone git@github.com:pampaparam/Gordeev.git
-apk add git
+cd root
+cd ~
 ls
-ls -la
-cat .ash_history
-echo $PWD
-ls -la
-git clone git@github.com:pampaparam/Gordeev.git
-cat ~/.ssh/id_rsa.pub > /dev/clipboard
-git clone git@github.com:pampaparam/Gordeev.git
-ls -la .ssh
-cat .ssh/id_rsa.pub
-cat ~/.ssh/id_rsa.pub > /dev/clipboard
-git clone git@github.com:pampaparam/Gordeev.git
-ls -la
+cd /
+sudo apt-get install git
+git version
+sudo apt update
+apt list --upgradeble
+apt list --upgradable
+sudo apt install nano
+nano --version
+lsof /mnt/my_share
+ls
+sudo lsof /mnt/my_share
+sudo lsof -e /run/user/1000/gvfs
+sudo lsof /mnt/my_share
+sudo apt upgrade
+sudo apt install telegram-desktop
+snap install telegram-desktop
+ls
+cd Documents
+ls
+cd ..
+ls
+cd Desktop
+ls
+touch PAT(classic).txt
+touch pat.txt
+ls
+nano pat.txt
+cat pat.txt
+cd ..
+ls
+git version
+git config --list
+git config --global
+clear
+git config --list
+cd /
+git config --list
+git status
+git clone https://token@github.com/pampaparam/Gordeev.git
+ls
+cd ..
+ls
+cd ~
+ls
+git clone git@github.com/pampaparam/Gordeev.git
+git clone https://token@github.com/pampaparam/Gordeev.git
+ls
+cd Gordeev
+ls
+cat README.md
+git config --global user.name "alexeygordeev"
+git config --global user.email "krevetkanos@gmail.com"
+git config --list
+cd ..
+ls
+cd Gordeev
+s
+cd Gordeev
+git clone https://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@github.com/pampaparam/Gordeev.git
+q
+exit
+ls
 cd Gordeev
 ls -la
-apk add nano
-nano README.md
-cat README.md
- ~/.ashrc
-echo 'alias c="git add . && git commit -m upd && git push origin main"' > ~/.ashrc
-source ~/.ashrc
-git status
+s
+cat .ashrc
+cd ..
+cp ~/.ashrc ~/.profile
+sudo cp ~/.ashrc ~/.profile
+source ~/.profile
+s
 c
-git config --global user.name "Alexey Gordeev"
-git config --global user.email "krevetkanos@gmail.com"
+where is .profile
+find .profile
+where man
+where .profile
+ls -la
+cat .profile
+ls
+ls -la
+cat .bashrc
+clear
+cd Gordeev
+ls -la
+cat .ashrc
+cd ..
+cat .profile
+ls -la
+cp ~/Gordeev/.ashrc ~/.profile
+source ~/.profile
+cat .profile
+s
 c
-cp ~/.ash_history scripts.sh
+cd Gordeev
+s
+c
+s
+cd Gordeev
+s
+c
+ls -la
+cp ~/Gordeev/.ashrc ~/.profile
+source ~/.profile
+s
+c
+s
+cd Gordeev
+s
+c
+cp ~/Gordeev/.ashrc ~/.profile
+source ~/Gordeev/.ashrc
+s
+c
+ls -la
+s
+cd Gordeev
+s
+c
+cd ..
+cat .profile
+source ~/.profile
+s
+cd Gordeev
+c
+s
+cd Gordeev
+s
+c
+cat .ashrc
+cd ..
+cat .profile
+source ~.profile
+source ~/.profile
+s
+source ~/Gordeev/.ashrc
+s
+cd Gordeev
+s
+c
+source ~/.profile
+s
+cd Gordeev
+s
+. ~/profile
+cd ..
+. ~/profile
+. ~/.profile
+s
+cd Gordeev
+s
+c
+s
+v
+c
+source ~/.profile
+s
+sudo source ~/.profile
+sudo . ~/.profile
+ls -la
+chmod +r .profile
+ls -la
+source ~/.profile
+s
+cat .profile
+s
+sudo source ~/.profile
+cd Gordeev
+ls -la
+cd ..
+$SHELL
+echo $SHELL
+echo $PWD
+ls -la
+chmod +rw .profile
+ls -la
+source ~/.profile
+s
+cd Gordeev
+s
+lsof
+clear
