8b5ed666f7658c503dbfe151489950ab9f45045e
Gordeev Alexey <krevetkanos@gmail.com>
2025-11-01 18:30:33 +0800

    upd

--- clouds/ubuntu/stopdel.sh
+++ clouds/ubuntu/stopdel.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-
-for i in $(seq 1 5); do
-   yc compute instance stop my-vm
-   yc compute instance delete my-vm
-done
+yc compute instance stop "${1:-my-vm}"
+yc compute instance delete "${1:-my-vm}"
