199ca9ce1692440423493017550b9e4534237eb0
Gordeev Alexey <krevetkanos@gmail.com>
2025-11-01 19:34:42 +0800

    upd

--- ping.sh
+++ ping.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 URL="https://www.google.com"
 
-if curl -s --head "$URL" | head -n 1 |
+if curl -s --head "$URL" | head -n 1 | grep "200" > /dev/null; then
+   echo "сайт доступен"
+else
+   echo "Ошибка"
+fi
