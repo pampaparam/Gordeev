4ae3596b9811c15617da8ec53f216cd37e016983
Gordeev Alexey <krevetkanos@gmail.com>
2025-11-09 14:56:29 +0800

    upd

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -2,6 +2,6 @@ import telebot
 
 bot = telebot.Telebot('7369357860:AAHjVxgy1c0ZAtlh8nF2WAxcpXvwRAOJh6s')
 
-@bot.message_handlers(commands=['start'])
+@bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, 'Привет!')
