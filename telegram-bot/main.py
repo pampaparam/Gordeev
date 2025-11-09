bcc2e2da9b2c4e1083aa170f7f33d0719e903562
Gordeev Alexey <krevetkanos@gmail.com>
2025-11-09 21:40:27 +0800

    upd

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -4,8 +4,11 @@ bot = telebot.TeleBot('7369357860:AAHjVxgy1c0ZAtlh8nF2WAxcpXvwRAOJh6s')
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, 'Привет!')
+    bot.send_message(message.chat.id, message)
 
+@bot.message_handler(commands=['help'])
+def main(message):
+    bot.send_message(message.chat.id, '<b>Help</b> <em><u>information</u></em>', parse_mode='html')
 
 
 bot.polling(none_stop=True)
