157c3a446652f787b8e7b1b561978a21d7257e45
Gordeev Alexey <krevetkanos@gmail.com>
2025-11-09 17:02:18 +0800

    upd

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -5,3 +5,7 @@ bot = telebot.TeleBot('7369357860:AAHjVxgy1c0ZAtlh8nF2WAxcpXvwRAOJh6s')
 @bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, 'Привет!')
+
+
+
+bot.polling(none_stop=True)
