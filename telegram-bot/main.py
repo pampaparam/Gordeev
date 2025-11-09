a34fe17d95faf0a60f1cd64f44b2339844ca73b4
Gordeev Alexey <krevetkanos@gmail.com>
2025-11-09 22:29:17 +0800

    upd

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -3,8 +3,13 @@ import telebot
 bot = telebot.TeleBot('7369357860:AAHjVxgy1c0ZAtlh8nF2WAxcpXvwRAOJh6s')
 
 @bot.message_handler(commands=['start'])
-def main(message):
-    bot.send_message(message.chat.id, message)
+def main(message): 
+    send_long_message(bot, message.chat.id, message.text)
+    
+    for i in range(0, len(text), 4096):
+        bot.send_message(chat.id, text[i:i+4096])
+
+       
 
 @bot.message_handler(commands=['help'])
 def main(message):
