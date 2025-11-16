3fe3731f9763c4a09a7a341d63025f3894f9d286
Gordeev Alexey <krevetkanos@gmail.com>
2025-11-16 22:35:18 +0800

    upd

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -2,6 +2,15 @@ import telebot
 
 bot = telebot.TeleBot('7369357860:AAHjVxgy1c0ZAtlh8nF2WAxcpXvwRAOJh6s')
 
+m = title + "\n" + post
+if len(m) > 4095:
+    for x in range(0, len(m), 4095):
+        bot.reply_to(message, text=m[x:x+4095])
+else:
+    bot.reply_to(message, text=m)
+
+
+
 @bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, message)
