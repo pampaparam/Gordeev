774dedc70989f613138fd8e6d2c12679a572a8df
Gordeev Alexey <krevetkanos@gmail.com>
2025-12-24 18:15:56 +0900

    upd

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher
-from aiogram.types imo
+from aiogram.types import Message
+from aiogram.filters import Command
 
 
 BOT_TOKEN = "8430017466:AAEW71rfyYUZCAiZibHgISVj3w6glwebdNI"
@@ -7,3 +8,9 @@ BOT_TOKEN = "8430017466:AAEW71rfyYUZCAiZibHgISVj3w6glwebdNI"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
+
+
+@dp.message(Command("start"))
+async def process_start_command(message: Message):
+    await message.answer("Hello! I'm your friendly Telegram bot. How can I assist you today?")
