cdbca252acbd70bbfb5b061c2c5df7499c1da5f7
Gordeev Alexey <krevetkanos@gmail.com>
2026-01-24 02:38:41 +0900

    Simplify: minimal VM only (no backups)

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,41 +1,25 @@
-resource "yandex_compute_instance" "rest-api-vm1" {
-  name = "rest-api-vm1"
-  platform_id = "standard-v3"
-  zone = "${var.av_zone}"
-
-  resources {
-    cores = "2"
-    memory = "2"
-  }
-
-  boot_disk {
-    disk_id = yandex_compute_disk.boot-disk-vm1.id
-  }
-
-  network_interface {
-    subnet_id = "e2ltsu29c7ocvcu0bjr0"
-    nat = true
-  }
-
-  metadata = {
-    fqdn = "rest-api-vm1.${var.service_dns_zone}"
-    ssh-keys = "debian:${file("~/.ssh/id_rsa.pub")}"
-  }
-
-  connection {
-    host = self.network_interface[0].nat_ip_address
-    type = "ssh"
-    user = "debian"
-    private_key = file("~/.ssh/id_rsa")
-  }
-
-  provisioner "remote-exec" {
-    script = "scripts/wait-vm.sh"
-  }
-
-  provisioner "local-exec" {
-  command = "cd ../provision && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${self.network_interface[0].nat_ip_address},' vm1.yml"
-  when    = create
-  }
-
+resource "yandex_vpc_network" "net" {
+  name = "bot-net"
 }
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "bot-subnet"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.net.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_compute_instance" "bot-vm" {
+  name = "telegram-bot-vm"
+  zone = "ru-central1-b"
+  
+  resources {
+    cores  = 2
+    memory = 2
+  }
+  
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.debian.id
+    }
+  }
