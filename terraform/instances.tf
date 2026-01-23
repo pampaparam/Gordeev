1fafea5361d4349b3393fc5c2e22df339433e09e
Gordeev Alexey <krevetkanos@gmail.com>
2026-01-24 02:52:07 +0900

    upd

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -8,19 +8,3 @@ resource "yandex_vpc_subnet" "subnet" {
   network_id     = yandex_vpc_network.net.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
-
-resource "yandex_compute_instance" "bot-vm" {
-  name = "telegram-bot-vm"
-  zone = "ru-central1-b"
-}
-  
-  resource {
-    cores  = 2
-    memory = 2
-  }
-  
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.debian.id
-    }
-  }
